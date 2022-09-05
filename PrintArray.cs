9622194c5eb93e0dcc7654d88825c9936932b9ac
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-05 14:14:32 +0530

    Added PrintArray class

---
+++ PrintArray.cs
@@ -0,0 +1,13 @@
+﻿namespace SerachAndSortAlogrithms
+{
+    public class PrintArray
+    {
+        public static void Print<T>(T[] array)
+        {
+            Console.WriteLine();
+            foreach (T item in array)
+                Console.Write("{0} -> ",item);
+            Console.Write("END \n");
+        }
+    }
+}
