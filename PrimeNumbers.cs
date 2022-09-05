d54da193414ce18b1179f47038137a81abaef139
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-05 14:14:26 +0530

    Added PrimeNumbers class

---
+++ PrimeNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace SerachAndSortAlogrithms
+{
+    public class PrimeNumbers
+    {
+        public static int[] GetPrimeNumbers(int range)
+        {
+            var primeNumbersList = new List<int>();
+
+            for (int i = 2; i <= range; i++)
+            {
+                var shouldAdd = 0;
+                for (int j = 2; j < i / 2 + 1; j++)
+                {
+                    if (i % j == 0)
+                        shouldAdd = 1;
+                }
+                if (shouldAdd == 0)
+                    primeNumbersList.Add(i);
+            }
+
+            int[] primeNumbers = primeNumbersList.ToArray();
+
+            return primeNumbers;
+        }
+    }
+}
